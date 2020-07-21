3902110443ae1d6894d3ac174747fc5cd20f6c25
mm <mmick@protonmail.com>
2020-07-21 21:15:40 +0200

    move.bat: improve batch

--- move.bat
+++ move.bat
@@ -1,2 +1,4 @@
-copy C:\Users\test\Desktop\KPTemp\target\x86_64-pc-windows-msvc\release\kp_temp.exe C:\Users\test\Desktop\kp_temp\64bits\kp_temp_64.exe
-copy C:\Users\test\Desktop\KPTemp\target\i686-pc-windows-msvc\release\kp_temp.exe C:\Users\test\Desktop\kp_temp\32bits\kp_temp_32.exe
+mkdir %userprofile%\Desktop\kp_temp\64bits
+mkdir %userprofile%\Desktop\kp_temp\32bits
+copy %userprofile%\Desktop\KPTemp\target\x86_64-pc-windows-msvc\release\kp_temp.exe %userprofile%\Desktop\kp_temp\64bits\kp_temp_64.exe
+copy %userprofile%\Desktop\KPTemp\target\i686-pc-windows-msvc\release\kp_temp.exe %userprofile%\Desktop\kp_temp\32bits\kp_temp_32.exe
